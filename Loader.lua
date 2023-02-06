823c5725fc7c92888ed1b3bccd62400d5fa87f77
66-75-63-6B-79-6F-75 <109988692+66-75-63-6B-79-6F-75@users.noreply.github.com>
2023-02-06 15:28:32 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,7 @@ function checkPlaceId(placeId, url)
     if game.PlaceId == placeId then
       loadstring(game:HttpGet(url))()
     else
-      print("Place Id does not match")
+      return "Place Id does not match"
     end
 end
   
